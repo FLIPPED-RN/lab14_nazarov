6a160625d519d439fcf7209961d867fac44b4544
Nazarov Ruslan <rnazarov142@gmail.com>
2023-11-15 17:37:50 +0200

    Добавьте файлы проекта.

---
+++ lab14_nazarov/Program.cs
@@ -0,0 +1,16 @@
+﻿//Средний уровень, Вариант 23
+
+using System.Text;
+
+Console.Write("Введите строку: ");
+string str = Console.ReadLine();
+
+StringBuilder result = new StringBuilder();
+
+for (int i = 0; i < str.Length; i++)
+{
+    result.Append(str[i]);
+    result.Append((" = " + (int)str[i]).ToString() + " ");
+}
+
+Console.WriteLine(result);
